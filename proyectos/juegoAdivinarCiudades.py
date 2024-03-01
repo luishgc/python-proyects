f6b0b76d52b9c7507ea5790e4b6077c2c99c0869
luis <lhgc2014@yahoo.com>
2024-03-01 18:13:25 -0500

    se agrego juego advinar  ciudades

---
+++ proyectos/juegoAdivinarCiudades.py
@@ -0,0 +1,22 @@
+import random
+
+palabras = ["cusco","arequipa","lima","ica"]
+palabraRandom = random.choice(palabras)
+palabraVacia = "_" * len(palabraRandom)
+palabraLista = list(palabraVacia)
+intentos = 1
+while True:
+    letra = input("Ingresa letra: ").lower()
+    if letra in palabraRandom:
+        for i in range(len(palabraRandom)):
+            if palabraRandom[i] == letra:
+                palabraLista[i] = letra
+        print("Adivina la palabra: "+"".join(palabraLista))
+        if "_" not in palabraLista:
+            print("Felicitaciones")
+            break                             
+    else:
+        print("Intenta con otra letra")
+    intentos += 1
+print("Lo conseguiste en: ", intentos, "intentos")
+
